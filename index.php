7290a61587acdc8f4304c17f862f748838596198
Homer Salazar <124323352+Homer-salazar@users.noreply.github.com>
2023-07-11 08:35:01 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -61,20 +61,20 @@
                             </div>
                             <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium max-sm:bg-[var(--secondary)] border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
                                 <li>
-                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-[var(--primary)] md:hover:text-blue-700 md:p-0">Home</a>
+                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-[var(--primary)] md:hover:text-[var(--primary)] md:p-0">Home</a>
                                 </li>
                                 <li>
-                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-[var(--primary)] md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Recipes</a>
+                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-[var(--primary)] md:hover:bg-transparent md:hover:text-[var(--primary)] md:p-0">Recipes</a>
                                 </li>
                                 <li>
-                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-[var(--primary)] md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</a>
+                                    <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-[var(--primary)] md:hover:bg-transparent md:hover:text-[var(--primary)] md:p-0">Contact</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </nav>
                 <section>
-                    <div class="flex justify-center items-center mt-10 h-[60vh]">
+                    <!-- <div class="flex justify-center items-center mt-10 h-[60vh]">
                         <div class="grid md:grid-cols-2 grid-cols-1 gap-4">
                             <div class="max-sm:order-2">
                                 <h1 class="text-6xl font-bold text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl"><?=$mealDB->mealName; ?></h1>
@@ -88,87 +88,85 @@
                                 <img class="w-72 mx-auto max-w-full rounded-lg shadow-lg ring-2 ring-[var(--secondary)]" src="<?=$mealDB->mealThumb ?>" alt="">
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </section>
             </div>
-            <div class="recommendation h-screen p-5">
-                <div class="grid grid-cols-2 max-sm:grid-cols-1 place-items-center">
-                    <?php 
-                        foreach (range(1, 1) as $i) {
-                        $randomMeal = $mealDB->getRandomMeal();
-                    ?>
-                        <div class="max-w-sm border border-gray-200 rounded-lg shadow dark:bg-[var(--accent)] dark:border-[var(--secondary)]">
-                            <div>
-                                <img class="rounded-t-lg md:h-auto" src="<?=$mealDB->mealThumb ?>" alt="" />
-                            </div>
-                            <div class="p-5">
-                                <a href="#">
-                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?=$mealDB->mealName; ?></h5>
-                                </a>
-                                <p class="mb-3 font-normal"><?=$mealDB->mealArea ?> &#x2022; <?=$mealDB->mealCategory ?></p>
-                                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-[var(--secondary)] rounded-lg hover:bg-[var(--secondary)] focus:ring-4 focus:outline-none focus:ring-[var(--secondary)]">
-                                    Read more
-                                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        <?php 
-                            }
-                        ?>
-                    </div>
-                    <div class="grid grid-rows-3 gap-4 pt-11 mySwiper">
-                        <?php 
-                            for ($i = 0; $i < 3; $i++) {
-                                $randomMeal = $mealDB->getRandomMeal();
-                        ?>
-                            <div class="flex flex-col swiper-slide items-center dark:bg-[var(--accent)] border border-[var(--secondary) rounded-lg shadow md:flex-row md:max-w-xl">
-                                <img class="object-cover w-full rounded-t-lg h-82 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="<?=$mealDB->mealThumb ?>" alt="">
-                                <div class="flex flex-col justify-between p-4 leading-normal">
-                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?=$mealDB->mealName; ?></h5>
-                                    <p class="mb-3 font-normal"><?=$mealDB->mealArea ?> &#x2022; <?=$mealDB->mealCategory ?></p>
-                                    <a href="#" class="inline-flex w-32  px-3 py-2 text-sm font-medium text-center bg-[var(--secondary)] rounded-lg hover:bg-[var(--secondary)] focus:ring-4 focus:outline-none focus:ring-[var(--secondary)]">
-                                        Read more
-                                        <svg class="w-3.5 h-3.5 mt-1 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                                        </svg>
-                                    </a>
-                                </div>
-                            </div>
-                        <?php } ?>
-                    </div>
+            <div class="recommendation h-screen p-2">
+                <div class="grid md:place-items-center">
+                    <h1 class="font-bold max-sm:text-xl text-4xl mb-3 mt-1">Today's Recommendation</h1>
                 </div>
+                <!-- <div class="card-section overflow-hidden p-3" style="scrollbar-width: thin;">
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper mt-2 gap-2 cursor-pointer">
+                            <?php 
+                                for ($i = 0; $i < 10; $i++) { 
+                                $randomMeal = $mealDB->getRandomMeal();
+
+                            ?>
+                                <div class="swiper-slide">
+                                    <div class="max-w-sm border border-gray-200 rounded-lg shadow dark:bg-[var(--accent)] dark:border-[var(--secondary)]">
+                                        <div>
+                                            <img class="rounded-t-lg md:h-auto" src="<?=$mealDB->mealThumb ?>" alt="" />
+                                        </div>
+                                        <div class="p-5">
+                                            <a href="#">
+                                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:><?=$mealDB->mealName; ?></h5>
+                                            </a>
+                                            <p class="mb-3 font-normal"><?=$mealDB->mealArea ?> &#x2022; <?=$mealDB->mealCategory ?></p>
+                                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-[var(--secondary)] rounded-lg hover:bg-[var(--secondary)] focus:ring-4 focus:outline-none focus:ring-[var(--secondary)]">
+                                                Read more
+                                                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                                </svg>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div> -->
             </div>
-            <div class="kitchen h-screen">
-                <div class="grid grid-cols-4  mt-5 gap-2 p-2">
-                    <div class="bg-[var(--tc)] p-3 text-center rounded-lg overflow-hidden shadow-md md:m-5 hover:bg-[var(--sc)]">
-                        <div class="flex flex-col items-center">
-                            <span class="mb-2"><i class="fas fa-carrot fa-2x"></i></span>
-                            <span class="max-sm:hidden font-bold text-xl">Vegetable</span>
-                        </div>        
-                    </div>
-                    <div class="bg-[var(--tc)] p-3 text-center rounded-lg overflow-hidden shadow-md md:m-5 hover:bg-[var(--sc)]">
-                        <div class="flex flex-col items-center">
-                            <span class="mb-2"><i class="fas fa-bacon fa-2x"></i></span>
-                            <span class="max-sm:hidden font-bold text-xl">Beef</span>
-                        </div>   
-                    </div>
-                    <div class="bg-[var(--tc)] p-3 text-center rounded-lg overflow-hidden shadow-md md:m-5 hover:bg-[var(--sc)]">
-                        <div class="flex flex-col items-center">
-                            <span class="mb-2"><i class="fas fa-fish fa-2x"></i></span>
-                            <span class="max-sm:hidden font-bold text-xl">Seafood</span>
-                        </div>   
-                    </div>
-                    <div class="bg-[var(--tc)] p-3 text-center rounded-lg overflow-hidden shadow-md md:m-5 hover:bg-[var(--sc)]">
-                        <div class="flex flex-col items-center">
-                            <span class="mb-2"><i class="fas fa-drumstick-bite fa-2x"></i></span>
-                            <span class="max-sm:hidden font-bold text-xl">Chicken</span>
-                        </div>   
-                    </div>
+            <div class="category h-[20vh] p-4">
+                <h1 class="pb-2">Categories</h1>
+                <div class="grid grid-cols-7">
+                    <button type="button" class="bg-[var(--primary)] hover:bg-[var(--secondary)] focus:ring-4 focus:ring-[var(--secondary)] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+                        <i class="fa-solid fa-drumstick-bite fa-2x"></i><br>
+                        <span class="text-2xl">
+                            Chicken
+                        </span>
+                    </button>
+                    <button type="button" class="bg-[var(--primary)] hover:bg-[var(--secondary)] focus:ring-4 focus:ring-[var(--secondary)] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Default</button>
+                    <button type="button" class="bg-[var(--primary)] hover:bg-[var(--secondary)] focus:ring-4 focus:ring-[var(--secondary)] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Default</button>
+                    <button type="button" class="bg-[var(--primary)] hover:bg-[var(--secondary)] focus:ring-4 focus:ring-[var(--secondary)] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Default</button>
+                    <button type="button" class="bg-[var(--primary)] hover:bg-[var(--secondary)] focus:ring-4 focus:ring-[var(--secondary)] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Default</button>
+                    <button type="button" class="bg-[var(--primary)] hover:bg-[var(--secondary)] focus:ring-4 focus:ring-[var(--secondary)] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Default</button>
+                    <button type="button" class="bg-[var(--primary)] hover:bg-[var(--secondary)] focus:ring-4 focus:ring-[var(--secondary)] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Default</button>
                 </div>
             </div>
         </div>
-        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+v
+        <!-- Swiper JS -->
+        <script src="https://unpkg.com/swiper@6.8.4/swiper-bundle.min.js"></script>
+        <!-- Initialize Swiper -->
+        <script>
+            var swiper = new Swiper('.swiper-container', {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                breakpoints: {
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 5,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                },
+            });
+        </script>   
+
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
     </body>
